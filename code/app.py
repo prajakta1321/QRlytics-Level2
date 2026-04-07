23a18e8ad630ebdad5e6abd9adebc9bcb4578205
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-04-07 09:59:14 +0530

    Update app.py

--- code/app.py
+++ code/app.py
@@ -3,3 +3,6 @@ import streamlit as st
 from qr_generator import generate_qr
 
 st.set_page_config(page_title = "SmartQR", page_icon = "")
+
+st.title('SmartQR Web Version')
+st.write('Generates QR codes instantly from text or urls')
