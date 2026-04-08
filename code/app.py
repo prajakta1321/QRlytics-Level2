02a4a600b178b2bb3ef19234ff8ee7718eff20a7
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-04-08 10:00:24 +0530

    Update app.py

--- code/app.py
+++ code/app.py
@@ -9,3 +9,21 @@ st.write('Generates QR codes instantly from text or urls')
 
 data = st.text_input('enter text or url.')
 
+if st.button('generate QR'):     # button to trigger qr generation
+    if not data.strip():         # check if input is empty
+        st.error('input cannot be empty.')
+    else:
+        try:
+            filename = generate_qr(data)  # generatre qr and get the filename
+            st.success('QR generated successfully:{filename}')
+            st.image(filename, caption = 'Generated qr code')   # to show image preview
+
+            with open(filename, 'rb') as file:
+                st.download_button(
+                    label = 'Download qr',
+                    data = file,
+                    file_name = filename,
+                    mime = 'image/png'
+                )
+        except Exception as e:
+            st.error(f'error:{e}')
