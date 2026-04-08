70303460fc4a498364e4bcfde5e1c94b98ec6f47
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-04-08 10:00:01 +0530

    Update app.py

--- code/app.py
+++ code/app.py
@@ -6,3 +6,6 @@ st.set_page_config(page_title = "SmartQR", page_icon = "")
 
 st.title('SmartQR Web Version')
 st.write('Generates QR codes instantly from text or urls')
+
+data = st.text_input('enter text or url.')
+
