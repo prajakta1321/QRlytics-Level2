f99312e91508fc57a2077c37ab18c0434c0074e5
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-04-09 09:37:32 +0530

    Create main.py

---
+++ code/main.py
@@ -0,0 +1,2 @@
+from qr_generator import generate_qr               # Import generate_qr function from qr_generator file
+from utils import is_valid_url                     # Import URL validation function (used later if needed)
