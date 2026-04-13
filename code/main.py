5116f69530bdd6054bbeaec0c6a4fd1a331e4eee
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-04-13 09:30:16 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,2 +1,8 @@
 from qr_generator import generate_qr               # Import generate_qr function from qr_generator file
 from utils import is_valid_url                     # Import URL validation function (used later if needed)
+
+def main():                                        # main function that runs the CLI interface, handles user interaction and calls qr generator
+    print("\n Welcome to SmartQR - CLI Version \n")
+
+    data = input("Enter text or URL to generate QR: ").strip()
+                                                   # .strip() removes unnecessary spaces from beginning and end
